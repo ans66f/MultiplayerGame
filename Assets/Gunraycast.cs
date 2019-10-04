f20904b470d4b601448280e91661f2a20e096594
ans66f <andrew.n.scott@hotmail.com>
2019-10-04 11:53:26 +0100

    Firstperson Right click to aim gun

---
+++ Assets/Gunraycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunraycast : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Debug.DrawRay(gameObject.transform.position, (gameObject.transform.forward * 100), Color.red, 1);
+
+
+
+        int i = 0;
+        Vector3[] linepositions;
+        linepositions = new Vector3[10];
+        for(i = 0; i < 10; i++)
+        {
+            linepositions[i] = Vector3.Lerp(gameObject.transform.position, (gameObject.transform.forward * 100), i);
+            
+        }
+        GetComponent<LineRenderer>().SetPositions(linepositions);
+
+    }
+}
---
+++ Assets/Lerptoaimposition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerptoaimposition : MonoBehaviour
+{
+    public GameObject aimpositionobject;
+    public GameObject holdpositionobject;
+
+
+    float lerpamount = 0;
+    float lerptime = 0.5f;
+    float l = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        l = lerpamount / lerptime;
+
+
+        if(lerpamount >= lerptime)
+        {
+            lerpamount = lerptime;
+        }
+        if(lerpamount <= 0)
+        {
+            lerpamount = 0;
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            Debug.Log("isdown");
+            lerpamount += Time.deltaTime;
+
+        }
+        else
+        {
+            lerpamount -= Time.deltaTime;
+        }
+        gameObject.transform.position = Vector3.Lerp(holdpositionobject.transform.position, aimpositionobject.transform.position, l);
+
+
+    }
+}
