8b6a9e5971d8a98e9efd8a811d9fa3c93c22b25c
Gazzap61 <56123435+Gazzap61@users.noreply.github.com>
2019-10-10 17:39:59 +0100

    Removed Sync

--- Assets/Experimental folder/Player.cs
+++ Assets/Experimental folder/Player.cs
@@ -8,46 +8,46 @@ using UnityEngine;
 public class Player : Photon.MonoBehaviour
 {
     public float speed = 10f;
-    private float lastSynchronizationTime = 0f;
-    private float syncDelay = 0f;
-    private float syncTime = 0f;
-    private Vector3 syncStartPosition = Vector3.zero;
-    private Vector3 syncEndPosition = Vector3.zero;
+    //private float lastSynchronizationTime = 0f;
+    //private float syncDelay = 0f;
+    //private float syncTime = 0f;
+    //private Vector3 syncStartPosition = Vector3.zero;
+    //private Vector3 syncEndPosition = Vector3.zero;
        
-    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 
-    {
+    //{
 
-        if (stream.isWriting)
+    //    if (stream.isWriting)
 
-        {
-            stream.SendNext(GetComponent<Rigidbody>().position);
-            stream.SendNext(GetComponent<Rigidbody>().velocity);
-        }
+    //    {
+    //        stream.SendNext(GetComponent<Rigidbody>().position);
+    //        stream.SendNext(GetComponent<Rigidbody>().velocity);
+    //    }
 
-        else
+    //    else
 
-        {
+    //    {
 
-            Vector3 syncPosition = (Vector3)stream.ReceiveNext();
-            Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
-            syncTime = 0f;
-            syncDelay = Time.time - lastSynchronizationTime;
-            lastSynchronizationTime = Time.time;
-            syncEndPosition = syncPosition + syncVelocity * syncDelay;
-            syncStartPosition = GetComponent<Rigidbody>().position;
+    //        Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+    //        Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
+    //        syncTime = 0f;
+    //        syncDelay = Time.time - lastSynchronizationTime;
+    //        lastSynchronizationTime = Time.time;
+    //        syncEndPosition = syncPosition + syncVelocity * syncDelay;
+    //        syncStartPosition = GetComponent<Rigidbody>().position;
 
-        }
+    //    }
 
-    }
+    //}
 
 
 
-    private void Awake()
+    //private void Awake()
 
-    {
-        lastSynchronizationTime = Time.time;
-    }
+    //{
+    //    lastSynchronizationTime = Time.time;
+    //}
 
     public GameObject PlayerCam;
     public GameObject PlayerStuff;
@@ -91,7 +91,7 @@ void Update()
         else
 
         {
-            SyncedMovement();
+            //SyncedMovement();
             PlayerCam.tag = "Untagged";
             PlayerCam.SetActive(false);
         }
@@ -181,12 +181,12 @@ void Update()
 
     }
 
-    private void SyncedMovement()
+    //private void SyncedMovement()
 
-    {
-        syncTime += Time.deltaTime;
-        GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition,
-        syncEndPosition, syncTime / syncDelay);
-    }
+    //{
+    //    syncTime += Time.deltaTime;
+    //    GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition,
+    //    syncEndPosition, syncTime / syncDelay);
+    //}
 
 }
