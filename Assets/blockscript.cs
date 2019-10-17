7e12747c3baf1ca8c94b46f1e19e0cea19b63361
ans66f <andrew.n.scott@hotmail.com>
2019-10-17 18:02:12 +0100

    blocksynctest(error)

--- Assets/blockscript.cs
+++ Assets/blockscript.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class blockscript : Photon.MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    public void CallRemoveBlock()
-    {
-        photonView.RPC("RemoveBlock", PhotonTargets.All);
-    }
 
-    [PunRPC]
-    void RemoveBlock()
-    {
-        Debug.Log("Destroyed block :" + gameObject.transform.position);
-        Destroy(gameObject);
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+        // Update is called once per frame
+        void Update()
     {
         
     }
