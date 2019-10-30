a43ffbabd81ad2ba2e346035267e2c30a858a244
Gazzap61 <56123435+Gazzap61@users.noreply.github.com>
2019-10-30 13:52:38 +0000

    Working on Gun Purchasing

---
+++ Assets/Objects/Weapons/WeaponRotate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRotate : Photon.MonoBehaviour
+{
+    float speed = 50.0f;
+    public GameObject Weapon;
+    public int WeaponCost;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+
+    void DoDestroyWeaponAndThis()
+    {
+        photonView.RPC("DestroyWeaponAndThis", PhotonTargets.AllBuffered);
+    }
+
+    [PunRPC]
+    void DestroyWeaponAndThis()
+    {
+        Weapon.SetActive(false);
+        GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y - 1000, GetComponent<Transform>().position.z);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Weapon.GetActive())
+        {
+            if (other.tag == "Player")
+            {
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (other.gameObject.GetComponent<Player>().currMoney >= WeaponCost)
+                    {
+                        other.gameObject.GetComponent<Player>().DoModifyMoney(other.gameObject.GetComponent<Player>().currMoney - WeaponCost);
+
+                        DoDestroyWeaponAndThis();
+
+                    }
+                    else
+                    {
+                        Debug.Log("Not enough money skrub, need: " + (WeaponCost - other.gameObject.GetComponent<Player>().currMoney) + " more");
+                    }
+                }
+            }
+        }
+    }
+
+}
