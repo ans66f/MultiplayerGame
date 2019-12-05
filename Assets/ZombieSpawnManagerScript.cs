594f38f4c6766c8ee1a3a5e4e9985be4ace19e35
ans66f <andrew.n.scott@hotmail.com>
2019-12-05 17:12:01 +0000

    waves start

--- Assets/ZombieSpawnManagerScript.cs
+++ Assets/ZombieSpawnManagerScript.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ZombieSpawnManagerScript : MonoBehaviour
 {
 
     public List<GameObject> Gates;
     GameObject[] EnemySpawners;
-    
+
+
+    GameObject ThisPlayer;
+
+    public GameObject ZombieTextObject;
+
+    /* // Optimisation ?? maybe
+    public int CurrentZombie = 0;
+    public int MaxCurrentZombie = 10;
+    */
+
+    int InitialZombieNum = 5;
+
+    int CurrentZombieStorage = 5;
+    int Wavenum = 1;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +36,39 @@ public class ZombieSpawnManagerScript : MonoBehaviour
 
     }
 
+    public bool TrySpawnZombie()
+    {
+
+        if((CurrentZombieStorage - 1) >= 0)
+        {
+            CurrentZombieStorage -= 1;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } 
+
     // Update is called once per frame
     void Update()
     {
+        if (ThisPlayer == null)
+        {
+            GameObject[] allplayers = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in allplayers)
+            {
+                if (p.GetPhotonView().isMine)
+                {
+                    ThisPlayer = p;
+                }
+
+            }
+        }
+
+
+
         int i = 0;
         int j = 0;
 
@@ -36,6 +83,35 @@ public class ZombieSpawnManagerScript : MonoBehaviour
                         EnemySpawners[j].GetComponent<enemyspawnscript>().IsSpawnerActive = true;
                     }
                 }
+            }
+        }
+
+
+        GameObject[] AllZombies = GameObject.FindGameObjectsWithTag("Enemy");
+        ZombieTextObject.GetComponent<Text>().text = "Zombies Left: " + (CurrentZombieStorage + AllZombies.Length) + " | Wave " + Wavenum;
+
+
+
+        
+        
+
+
+
+
+        if(CurrentZombieStorage <= 0 && AllZombies.Length <= 0)
+        {
+            Wavenum += 1;
+
+            InitialZombieNum += 5;
+            CurrentZombieStorage = InitialZombieNum;
+
+            if(!ThisPlayer.GetComponent<Player>().isDead)
+            {
+                ThisPlayer.GetComponent<Player>().DoModifyMoney((10 + InitialZombieNum));
+            }
+            else
+            {
+                ThisPlayer.GetComponent<Player>().isDead = false;
             }
         }
     }
